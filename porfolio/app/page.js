03153bc1bee22bcb72b411c21181d14856bb35b9
iroid2 <iradtu2@gmail.com>
2023-07-07 20:23:35 +0300

    inniial commit

--- porfolio/app/page.js
+++ porfolio/app/page.js
@@ -1,10 +1,19 @@
 import Navbar from "@/components/Navbar";
 import React from "react";
 import buttonInfor from "../script";
+import Image from "next/image";
 export default function page() {
   return (
     <div>
       <Navbar />
+      <div className="homeHero">
+        <div className="card">
+          <Image src={"/images/passp.jpg"} alt="" width={110} height={240} />
+          <h1>iroid</h1>
+          <p>Web Developer/ Accountant</p>
+          <button>Download Cv</button>
+        </div>
+      </div>
     </div>
   );
 }
