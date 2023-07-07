c0ff745790fd555426ceaa33e644ae45e872a39b
iroid2 <iradtu2@gmail.com>
2023-07-07 19:24:02 +0300

    inniial commit

---
+++ porfolio/app/about/page.js
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <Navbar />
+      <h1>About PAGE</h1>
+    </div>
+  );
+}
---
+++ porfolio/app/page.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+import buttonInfor from "../script";
+export default function page() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
