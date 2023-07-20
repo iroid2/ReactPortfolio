4c958238725d037ab35812cbd8ab4a44eeae12fe
iroid2 <iradtu2@gmail.com>
2023-07-20 14:52:22 +0300

    Addedsome few blogs

--- porfolio/components/Blog.jsx
+++ porfolio/components/Blog.jsx
@@ -2,57 +2,28 @@ import React from "react";
 import Heading from "./Heading";
 import Image from "next/image";
 import { posts } from "@/blogs";
+import Link from "next/link";
 export default function Blog() {
   console.log(posts);
   return (
     <div className="blogs-container globContainerPadding">
       <Heading title={"BLOGS"} />
+
       <div className="blogs-section">
-        <div className="blog">
-          <div className="blog-thumbnail">
-            <Image src={"/images/about.jpg"} alt="" width={200} height={100} />
-          </div>
-          <div className="blog-details">
-            <h1>How to get into Programming</h1>
-            <h2>Basics on what you have to consider</h2>
-            <h4>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus libero veritatis magnam obcaecati! Quae tenetur
-              blanditiis deserunt accusamus perspiciatis quidem corrupti
-              veritatis repudiandae asperiores fuga. Repellat, sunt ipsum!
-            </h4>
-          </div>
-        </div>
-        <div className="blog">
-          <div className="blog-thumbnail">
-            <Image src={"/images/about.jpg"} alt="" width={200} height={100} />
-          </div>
-          <div className="blog-details">
-            <h1>How to get into Programming</h1>
-            <h2>Basics on what you have to consider</h2>
-            <h4>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus libero veritatis magnam obcaecati! Quae tenetur
-              blanditiis deserunt accusamus perspiciatis quidem corrupti
-              veritatis repudiandae asperiores fuga. Repellat, sunt ipsum!
-            </h4>
-          </div>
-        </div>
-        <div className="blog">
-          <div className="blog-thumbnail">
-            <Image src={"/images/about.jpg"} alt="" width={200} height={100} />
-          </div>
-          <div className="blog-details">
-            <h1>How to get into Programming</h1>
-            <h2>Basics on what you have to consider</h2>
-            <h4>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus libero veritatis magnam obcaecati! Quae tenetur
-              blanditiis deserunt accusamus perspiciatis quidem corrupti
-              veritatis repudiandae asperiores fuga. Repellat, sunt ipsum!
-            </h4>
-          </div>
-        </div>
+        {posts.map((blog) => {
+          return (
+            <Link href={`/posts/${blog.slug}`} className="blog">
+              <div className="blog-thumbnail">
+                <Image src={blog.image} alt="" width={300} height={300} />
+              </div>
+              <div className="blog-details">
+                <h1>{blog.title}</h1>
+
+                <h3>{blog.description}</h3>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
